92a2fa39e5b1f77a1aa156ed8bc01cc55be7fdc3
Rachel Miller <millerre1984@gmail.com>
2015-06-22 19:50:11 +0000

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-  
+  def new
+    # form where user fills out their own profile.
+    @user = User.find( params[:user_id] )
+    @variable = params[:hello]
+    @profile = @user.build_profile
+  end
 end
